123b92ed8d2a5f1c317e3eca2e9159fc416002e9
metaleap <metaleap@users.noreply.github.com>
2019-04-16 16:05:59 +0200

    [autopush] «Turn right here. No! NO! The OTHER right!»

--- cmd/atem/main.go
+++ cmd/atem/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-leap/std"
 	"github.com/go-leap/sys"
+	"github.com/metaleap/atem"
 )
 
 var (
@@ -12,20 +13,36 @@ var (
 )
 
 func main() {
-	atcmd := usys.Arg(1)
+	atem.LibWatchInterval = ustd.FlagOfDuration("lib_watch_interval", atem.LibWatchInterval,
+		"    format: time-duration; sets how often to check all known atem libs-dirs for\n    file-modifications to reload accordingly. Disable with a zero duration.")
+
+	atcmd, showinfousage, showinfoargs := usys.Arg(1), false, false
 	switch atcmd {
-	case "help", "version", "run":
+	case "version", "run":
 		writeLns("command " + atcmd + " recognized but not yet implemented")
+	case "help":
+		showinfoargs, showinfousage = true, true
 	case "repl":
 		mainRepl()
 	case "fmt":
 		mainFmt()
 	default:
-		writeLns("Usage:",
-			"\tatem repl",
-			"\tatem fmt",
-			"\tatem run",
-			"\tatem help",
-			"\tatem version")
+		showinfousage = true
+	}
+
+	if showinfousage {
+		writeLns("", "Usage:", "",
+			"  atem repl",
+			"  atem fmt",
+			"  atem run",
+			"  atem help",
+			"  atem version", "")
+	}
+
+	if f := ustd.Flags; showinfoargs {
+		writeLns("", "Optional flags:", "")
+		for i := range f {
+			writeLns("  --"+f[i].Name+" (default: "+f[i].Default+")", f[i].Desc, "")
+		}
 	}
 }
