c5b2763f99a37205fd3d13af4e24407ca96295a7
metaleap <metaleap@users.noreply.github.com>
2019-06-20 15:50:37 +0200

    trouble-shooting

---
+++ tooling/lsp/notifications.go
@@ -0,0 +1,23 @@
+package atmolsp
+
+func OnInitialized() {}
+
+func OnExit() {}
+
+func OnCancelRequest() {}
+
+func WorkspaceOnDidChangeWorkspaceFolders() {}
+
+func WorkspaceOnDidChangeConfiguration() {}
+
+func WorkspaceOnDidChangeWatchedFiles() {}
+
+func TextDocumentOnDidOpen() {}
+
+func TextDocumentOnDidChange() {}
+
+func TextDocumentOnWillSave() {}
+
+func TextDocumentOnDidSave() {}
+
+func foo()
---
+++ tooling/lsp/requests.go
@@ -0,0 +1,17 @@
+package atmolsp
+
+import (
+	"github.com/metaleap/atmo/session"
+)
+
+var Ctx *atmosess.Ctx
+
+func Initialize() {}
+
+func Shutdown() {}
+
+func WorkspaceSymbols() {}
+
+func WorkspaceExecuteCommand() {}
+
+func TextDocumentWillSaveWaitUntil() {}
