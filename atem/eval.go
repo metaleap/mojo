3fffd426bfa45ef5e0afee5f7e48b15fd2be3f56
metaleap <metaleap@users.noreply.github.com>
2019-12-17 20:30:32 +0100

    the journey of battletesting begins

--- atem/eval.go
+++ atem/eval.go
@@ -1,6 +1,7 @@
 package atem
 
 import (
+	"fmt"
 	"os"
 	"time"
 )
@@ -76,12 +77,18 @@ var OpPrtDst = os.Stderr.Write
 // with a `[3]Expr` of first the `OpCode`-referencing `ExprFuncRef` followed
 // by both its operands.
 func (me Prog) Eval(expr Expr) Expr {
+	me[len(me)-1] = FuncDef{Meta: []string{"tmptest", "n"}, allArgsUsed: true, hasArgRefs: true,
+		Args: []int{1},
+		Body: ExprNumInt(12345),
+	}
+	expr = &ExprCall{Callee: ExprFuncRef(len(me) - 1), Args: []Expr{ExprNumInt(7)}}
+
 	CurEvalStepDepth, maxDepth = 0, 0
 	fnNumCalls = make(map[ExprFuncRef]int, len(me))
 	t := time.Now().UnixNano()
-	ret := me.eval(expr, nil)
+	ret := me.eval2(expr)
 	t = time.Now().UnixNano() - t
-	println(time.Duration(t).String(), "\t\t\t", maxDepth, maxStack, "\t\t", Count1, Count2, Count3, Count4)
+	println(fmt.Sprintf("%T", ret), time.Duration(t).String(), "\t\t\t", maxDepth, maxStack, "\t\t", Count1, Count2, Count3, Count4)
 	for fnr, num := range fnNumCalls {
 		println(num, "\tx\t", me[fnr].Meta[0])
 	}
@@ -94,9 +101,6 @@ var maxStack int
 var fnNumCalls map[ExprFuncRef]int
 
 func (me Prog) eval(expr Expr, curFnArgs []Expr) Expr {
-	if CurEvalStepDepth++; CurEvalStepDepth > maxDepth {
-		maxDepth = CurEvalStepDepth
-	}
 	for again := true; again; {
 		ondone := OnEvalStep(expr, curFnArgs)
 		again = false
@@ -228,66 +232,76 @@ func onEvalStepNoOp(Expr, []Expr) func(Expr, []Expr, bool) { return onEvalStepDo
 func onEvalStepDoneNoOp(Expr, []Expr, bool) {}
 
 func (me Prog) eval2(expr Expr) Expr {
-	type level struct { // each call adds a new level, when done it's dropped
-		stack    []Expr // first is callee, then rest are args
-		pos      int    // begins at end of `stack` and counts down to 0
+	// every call stacks a new `level` on top of lower ones, when call is done it's dropped.
+	// but there's always 1 root / base `level` for our `expr`
+	type level struct {
+		stash    []Expr // args in reverse order, then callee
+		pos      int    // begins at end of `stash` and counts down to 0
 		numArgs  int
 		argsDone bool
 	}
-
-	stacks := make([]level, 1, 1024)
-	stacks[0] = level{stack: append(make([]Expr, 0, 32), expr)}
+	levels := make([]level, 1, 1024)
+	levels[0].stash = append(make([]Expr, 0, 32), expr)
 
 	for {
-		cur := &stacks[len(stacks)-1]
+		cur := &levels[len(levels)-1]
+		println("levels", len(levels), "stash", len(cur.stash), "\tpos", cur.pos, "\t\targs", cur.argsDone, cur.numArgs)
+		if len(levels) > maxDepth {
+			maxDepth = len(levels)
+		}
+		if len(cur.stash) > maxStack {
+			maxStack = len(cur.stash)
+		}
 
 		for cur.pos < 0 {
-			if cur.argsDone {
-				cur.pos = len(cur.stack) - 1
-			} else if len(stacks) == 1 {
-				goto done
-			} else {
-				prev := &stacks[len(stacks)-2]
-				prev.stack[prev.pos], prev.pos = cur.stack[len(cur.stack)-1], prev.pos-1
-				cur, stacks = prev, stacks[:len(stacks)-1]
+			if cur.argsDone { // set at end-of-loop. now with all (used) args eval'd we jump back to callee
+				cur.pos = len(cur.stash) - 1
+			} else if len(levels) == 1 { // initial `expr` maximally reduced: return.
+				goto allDoneThusReturn
+			} else { // jump back up to prior level, dropping the current one
+				prev := &levels[len(levels)-2]
+				prev.stash[prev.pos], prev.pos = cur.stash[len(cur.stash)-1], prev.pos-1
+				cur, levels = prev, levels[:len(levels)-1]
 			}
 		}
 
-		switch it := cur.stack[cur.pos].(type) {
+		println(fmt.Sprintf("\t%T", cur.stash[cur.pos]))
 
-		// a will-be-discarded call-arg slot. we arrive here as our `pos` counts down, and keep that up:
+		switch it := cur.stash[cur.pos].(type) {
+
+		// a will-be-discarded call-arg slot. we arrive here as our `pos` counts down, and continue:
 		case nil:
 			cur.pos--
 
 		// a no-further-reducable final value, count down to "next" slot
 		case ExprNumInt:
-			cur.stack[cur.pos] = it
+			cur.stash[cur.pos] = it
 			cur.pos--
 
 		case ExprArgRef:
-			var stack []Expr
-			if cur.argsDone {
-				stack = cur.stack
-			} else {
-				stack = stacks[len(stacks)-2].stack
+			var stash []Expr
+			if cur.argsDone { // we're in callee and consume our own eval'd args
+				stash = cur.stash
+			} else { // we're eval'ing args so we consume parent already-eval'd args
+				stash = levels[len(levels)-2].stash
 			}
-			cur.stack[cur.pos] = stack[(len(stack)-1)+int(it)]
+			cur.stash[cur.pos] = stash[(len(stash)-1)+int(it)]
 
 		case *ExprCall:
 			if it.allArgsDone && it.IsClosure != 0 {
-				cur.pos-- // got to a no-further-reducable final value (closure)
+				cur.pos-- // we have a no-further-reducable final value (closure)
 			} else { // build up & add & enter the next `level`
 				callee, callargs := it.Callee, it.Args
 				for sub, isc := callee.(*ExprCall); isc; sub, isc = callee.(*ExprCall) {
 					callee, callargs = sub.Callee, append(callargs, sub.Args...)
 				}
-				stacks = append(stacks, level{pos: len(callargs), stack: append(callargs, callee)})
+				levels = append(levels, level{pos: len(callargs), stash: append(callargs, callee)})
 			}
 
 		case ExprFuncRef:
 			if it > 0 && len(me[it].Args) == 0 {
-				cur.stack[len(cur.stack)-1] = me[it].Body
-			} else if cur.pos == len(cur.stack)-1 && len(cur.stack) != 1 {
+				cur.stash[len(cur.stash)-1] = me[it].Body
+			} else if cur.pos == len(cur.stash)-1 && len(cur.stash) != 1 {
 				if !cur.argsDone {
 					cur.numArgs = 2
 					allargsused := true
@@ -295,21 +309,17 @@ func (me Prog) eval2(expr Expr) Expr {
 						cur.numArgs, allargsused = len(me[it].Args), me[it].allArgsUsed
 					}
 					if !allargsused {
-						for i := 0; i < cur.numArgs; i++ {
+						for i, idx := 0, len(cur.stash)-2; idx > -1 && i < cur.numArgs; i, idx = i+1, idx-1 {
 							if me[it].Args[i] == 0 {
-								if idx := len(cur.stack) - 2 - i; idx < 0 {
-									break
-								} else {
-									cur.stack[idx] = nil
-								}
+								cur.stash[idx] = nil
 							}
 						}
 					}
 					cur.pos--
-				} else if len(cur.stack) <= cur.numArgs {
+				} else if len(cur.stash) > cur.numArgs {
 					var result Expr
 					if it < 0 {
-						lhs, rhs := cur.stack[len(cur.stack)-3], cur.stack[len(cur.stack)-2]
+						lhs, rhs := cur.stash[len(cur.stash)-3], cur.stash[len(cur.stash)-2]
 						switch OpCode(it) {
 						case OpAdd:
 							result = lhs.(ExprNumInt) + rhs.(ExprNumInt)
@@ -342,7 +352,7 @@ func (me Prog) eval2(expr Expr) Expr {
 					} else {
 						result = me[it].Body
 					}
-					cur.stack[len(cur.stack)-1] = result
+					cur.stash[len(cur.stash)-1] = result
 					if _, isfnref := result.(ExprFuncRef); isfnref {
 						cur.pos--
 					}
@@ -354,26 +364,26 @@ func (me Prog) eval2(expr Expr) Expr {
 			}
 		}
 
-		if cur.numArgs != 0 && cur.pos > -1 {
-			if cur.argsDone && cur.pos < (len(cur.stack)-1) {
-				result := cur.stack[len(cur.stack)-1]
-				if diff := cur.numArgs - (len(cur.stack) - 1); diff > 0 {
-					result = &ExprCall{allArgsDone: true, IsClosure: diff, Callee: result, Args: cur.stack[:len(cur.stack)-1]}
-					cur.stack = []Expr{result}
+		if cur.numArgs != 0 && cur.pos < (len(cur.stash)-1) {
+			if cur.argsDone {
+				result := cur.stash[len(cur.stash)-1]
+				if diff := cur.numArgs - (len(cur.stash) - 1); diff > 0 {
+					result = &ExprCall{allArgsDone: true, IsClosure: diff, Callee: result, Args: cur.stash[:len(cur.stash)-1]}
+					cur.stash = []Expr{result}
 				} else {
-					cur.stack = append(cur.stack[:len(cur.stack)-1-cur.numArgs], result)
+					cur.stash = append(cur.stash[:len(cur.stash)-1-cur.numArgs], result)
 				}
-				cur.argsDone = false
-				if len(cur.stack) == 1 {
+				cur.argsDone, cur.numArgs = false, 0
+				if len(cur.stash) == 1 {
 					cur.pos = -1
 				} else {
-					cur.numArgs, cur.pos = 0, len(cur.stack)-1
+					cur.pos = len(cur.stash) - 1
 				}
-			} else if (!cur.argsDone) && cur.pos < (len(cur.stack)-1-cur.numArgs) {
+			} else if cur.pos < (len(cur.stash) - (1 + cur.numArgs)) {
 				cur.pos, cur.argsDone = -1, true
 			}
 		}
 	}
-done:
-	return stacks[0].stack[0]
+allDoneThusReturn:
+	return levels[0].stash[0]
 }
