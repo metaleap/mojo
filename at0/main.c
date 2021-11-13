81222ca930fbca58e1f9f1102a5f25e8ca07fde5
metaleap <metaleap@users.noreply.github.com>
2021-11-13 01:02:26 +0100

    [autopush] «May I ask a question?»

--- at0/main.c
+++ at0/main.c
@@ -1,8 +1,9 @@
-#define mem_bss_max (3 * 1024 * 1024)
-#include "utils_std_basics.c"
+#define mem_bss_max (16 * 1024 * 1024)
 
+#include "utils_std_basics.c"
+#include "utils_std_mem.c"
 
 
 int main () {
-    return 1;
+    return 0;
 }
